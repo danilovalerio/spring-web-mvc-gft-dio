b653d3ffd7e55773c4a2e805a2cc84239c43aff4
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-09-30 17:43:38 -0300

    lista todos jedi do repository

--- src/main/java/com/globallabs/springwebmvc/repository/JediRepository.java
+++ src/main/java/com/globallabs/springwebmvc/repository/JediRepository.java
@@ -1,4 +1,17 @@
 package com.globallabs.springwebmvc.repository;
 
+import com.globallabs.springwebmvc.model.Jedi;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class JediRepository {
+
+    public List<Jedi> getAllJedi(){
+        final List<Jedi> jedi = new ArrayList<>();
+
+        jedi.add(new Jedi("Luke", "SkyWalker"));
+        return jedi;
+    }
 }
